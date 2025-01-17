87ab693bfb7fb6ab03fbe5024f795cc5b2dbb2db
Simon Long <simon@raspberrypi.com>
2025-01-17 16:36:26 +0000

    Correct copyright

--- src/ejecter.c
+++ src/ejecter.c
@@ -37,7 +37,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "ejecter.h"
 
 /*----------------------------------------------------------------------------*/
-/* Typedefs and macros */
+/* Typedefs and macros                                                        */
 /*----------------------------------------------------------------------------*/
 
 #define DEBUG_ON
